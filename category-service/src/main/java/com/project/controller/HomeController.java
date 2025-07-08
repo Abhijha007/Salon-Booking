329320ae2359874382c143f8d8d917b3b28b946b
Abhijha007 <124693780+Abhijha007@users.noreply.github.com>
2025-07-08 20:04:17 +0530

    Update HomeController.java

--- category-service/src/main/java/com/project/controller/HomeController.java
+++ category-service/src/main/java/com/project/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+public class HomeController {           //class to test the api 
 
     @GetMapping
     public String HomeControllerHandler(){
