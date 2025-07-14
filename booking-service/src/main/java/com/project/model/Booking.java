766df48fe1d40676bf78c7ec0c3bca93770eb5fd
Abhijha007 <124693780+Abhijha007@users.noreply.github.com>
2025-07-14 20:21:42 +0530

    Update Booking.java

--- booking-service/src/main/java/com/project/model/Booking.java
+++ booking-service/src/main/java/com/project/model/Booking.java
@@ -1,4 +1,29 @@
 package com.project.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Data
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private long salonId;
+
+    private Long customerId;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+   @ElementCollection   //to create the separate table in the database
+    private Set<Long> serviceIds;
+
+   private int totalPrice;
 }
