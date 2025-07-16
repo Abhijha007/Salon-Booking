8f4a8cd20b075ff7bae98b475bf05ec92965b9e4
Abhijha007 <124693780+Abhijha007@users.noreply.github.com>
2025-07-16 20:22:11 +0530

    Update BookingRequest.java

--- booking-service/src/main/java/com/project/dto/BookingRequest.java
+++ booking-service/src/main/java/com/project/dto/BookingRequest.java
@@ -1,5 +1,11 @@
 package com.project.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
 public class BookingRequest {
     private LocalDateTime startTime;
     private LocalDateTime endTime;
