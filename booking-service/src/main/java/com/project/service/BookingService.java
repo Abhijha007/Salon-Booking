a92e4fbda7c9610d0577b69265de0ff7bb7bf41a
Abhijha007 <124693780+Abhijha007@users.noreply.github.com>
2025-07-16 20:19:45 +0530

    Update BookingService.java

--- booking-service/src/main/java/com/project/service/BookingService.java
+++ booking-service/src/main/java/com/project/service/BookingService.java
@@ -1,4 +1,13 @@
 package com.project.service;
 
+import com.project.dto.BookingRequest;
+import com.project.dto.SalonDTO;
+import com.project.dto.ServiceDTO;
+import com.project.dto.UserDTO;
+import com.project.model.Booking;
+
+
 public interface BookingService {
+   Booking createBooking(BookingRequest booking, UserDTO user,
+                          SalonDTO salon, Set<ServiceDTO> serviceDTOSet);
 }
