bfe39816c9cd36b6544591556d4df3e04f244a59
Abhijha007 <124693780+Abhijha007@users.noreply.github.com>
2025-07-18 20:08:55 +0530

    Update BookingRepository.java

--- booking-service/src/main/java/com/project/repository/BookingRepository.java
+++ booking-service/src/main/java/com/project/repository/BookingRepository.java
@@ -3,5 +3,9 @@ package com.project.repository;
 import com.project.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+  List<Booking> findByCustomerId(Long customerId);
+  List<Booking> findBySalonId(Long salonId);
 }
