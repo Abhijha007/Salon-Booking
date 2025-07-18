545383a2534924811a970ec7c8110ca0d0b9e0e6
Abhijha007 <124693780+Abhijha007@users.noreply.github.com>
2025-07-18 20:03:26 +0530

    Update BookingServiceImpl.java

--- booking-service/src/main/java/com/project/service/impl/BookingServiceImpl.java
+++ booking-service/src/main/java/com/project/service/impl/BookingServiceImpl.java
@@ -4,4 +4,40 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BookingServiceImpl implements BookingService{
+
+  
+  @Override
+    public Booking createBooking(BookingRequest booking, UserDTO user, SalonDTO salon, Set<ServiceDTO> serviceDTOSet) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> getBookingsByCustomer(Long customerId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Booking> getBookingsBySalon(Long salonId) {
+        return List.of();
+    }
+
+    @Override
+    public Booking getBookingById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Booking updateBooking(Long bookingId, BookingStatus status) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> getBookingByDate(LocalDate date, Long salonId) {
+        return List.of();
+    }
+
+    @Override
+    public SalonReport getSalonReport(Long salonId) {
+        return null;
+    }
 }
