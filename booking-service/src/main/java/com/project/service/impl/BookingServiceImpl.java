a22d4b6d9e0ea6d8ce842303334d095b3274d7d7
Abhijha007 <124693780+Abhijha007@users.noreply.github.com>
2025-07-22 19:34:19 +0530

    Update BookingServiceImpl.java

--- booking-service/src/main/java/com/project/service/impl/BookingServiceImpl.java
+++ booking-service/src/main/java/com/project/service/impl/BookingServiceImpl.java
@@ -36,6 +36,12 @@ public class BookingServiceImpl implements BookingService{
         return null;
     }
 
+  public Boolean isTimeSlotAvailable(SalonDTO salonDTO,
+                                       LocalDateTime bookingStartTime,
+                                       LocalDateTime bookingEndTime){
+        return true;
+    }
+
     @Override
     public List<Booking> getBookingsByCustomer(Long customerId) {
         return List.of();
