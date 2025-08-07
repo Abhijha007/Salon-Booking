e7cd388951a52f18644c8d986a80afa67426dec8
Abhijha007 <124693780+Abhijha007@users.noreply.github.com>
2025-08-07 18:21:31 +0530

    Update BookingController.java

--- booking-service/src/main/java/com/project/controller/BookingController.java
+++ booking-service/src/main/java/com/project/controller/BookingController.java
@@ -1,4 +1,21 @@
 package com.project.controller;
 
+import com.project.dto.BookingRequest;
+import com.project.dto.SalonDTO;
+import com.project.dto.UserDTO;
+import com.project.model.Booking;
+import com.project.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bookings")
+@RequiredArgsConstructor
 public class BookingController {
+  
 }
+
