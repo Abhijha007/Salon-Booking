f243be53bd3ad45510aa1477f93dbab5b4e1ee61
Abhijha007 <124693780+Abhijha007@users.noreply.github.com>
2025-08-11 18:17:17 +0530

    Update BookingController.java

--- booking-service/src/main/java/com/project/controller/BookingController.java
+++ booking-service/src/main/java/com/project/controller/BookingController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/bookings")
@@ -44,6 +45,7 @@ public class BookingController {
     return null;
             }
 }
+
 
 
 
