a91ebb9eec2d623fbee88ea39e1b342fa754ebb3
Abhijha007 <124693780+Abhijha007@users.noreply.github.com>
2025-08-15 17:59:33 +0530

    Update BookingMapper.java

--- booking-service/src/main/java/com/project/mapper/BookingMapper.java
+++ booking-service/src/main/java/com/project/mapper/BookingMapper.java
@@ -1,4 +1,15 @@
 package com.project.mapper;
 
 public class BookingMapper {
+  public static BookingDTO toDTO(Booking booking) {
+        BookingDTO bookingDTO = new BookingDTO();
+        bookingDTO.setId(booking.getId());
+        bookingDTO.setCustomerId(booking.getCustomerId());
+        bookingDTO.setStatus(booking.getStatus());
+        bookingDTO.setEndTime(booking.getEndTime());
+        bookingDTO.setSalonId(bookingDTO.getSalonId());
+        bookingDTO.setServiceIds(booking.getServiceIds());
+        return bookingDTO;
+    }
 }
+
