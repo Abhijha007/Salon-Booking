83118b149147ea575bc06e6fd5c222a0122eeb5f
Abhijha007 <124693780+Abhijha007@users.noreply.github.com>
2025-08-30 23:16:17 +0530

    Update BookingSlotDTO.java

--- booking-service/src/main/java/com/project/dto/BookingSlotDTO.java
+++ booking-service/src/main/java/com/project/dto/BookingSlotDTO.java
@@ -1,4 +1,13 @@
 package com.project.dto;
+import lombok.Data;
+
+import java.time.LocalDateTime;
 
 public class BookingSlotDTO {
+
+@Data
+public class BookingSlotDTO {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
+
